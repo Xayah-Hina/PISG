bb3bdf5122caec7fcc439b506836e5658bdbd235
Xayah <ycxiang14sep@gmail.com>
2025-02-19 21:29:56 +0900

    reconstruct structures

--- src/pipeline.py
+++ src/pipeline.py
@@ -46,6 +46,160 @@ find_relative_paths(training_videos)
 find_relative_paths(camera_calibrations)
 
 
+def resample_images_by_ratio_device(images: torch.Tensor, ratio: float):
+    """
+    resample images by ratio
+
+    Args:
+    - images: torch.Tensor of shape (V, T, H, W, C)
+    - ratio: float, resampling ratio
+
+    Returns:
+    - torch.Tensor of shape (V, T, H * ratio, W * ratio, C)
+
+    """
+    V, T, H, W, C = images.shape
+    images_permuted = images.permute(0, 1, 4, 2, 3).reshape(V * T, C, H, W)  # (V * T, C, H, W)
+    images_permuted_resized = (torch.nn.functional.interpolate(images_permuted, size=(int(H * ratio), int(W * ratio)), mode='bilinear', align_corners=False)
+                               .reshape(V, T, C, int(H * ratio), int(W * ratio))
+                               .permute(0, 1, 3, 4, 2))  # (V, T, H * ratio, W * ratio, C)
+    return images_permuted_resized
+
+
+def resample_frames(frames: torch.Tensor, u: torch.Tensor, v: torch.Tensor):
+    """
+    Resample frames using the given UV coordinates.
+
+    Args:
+    - frames: torch.Tensor of shape (..., H, W, C)
+    - u: torch.Tensor of shape (N, H, W)
+    - v: torch.Tensor of shape (N, H, W)
+
+    Returns:
+    - resampled_images: torch.Tensor of shape (N, T, H, W, C)
+    """
+
+    H, W, C = frames.shape[-3:]
+    u_norm, v_norm = 2.0 * (u / (W - 1)) - 1, 2.0 * (v / (H - 1)) - 1
+    grid = torch.stack((u_norm, v_norm), dim=-1).unsqueeze(0)  # (1, H, W, 2)
+    orig_shape = frames.shape
+    reshaped_images = frames.reshape(-1, H, W, C).permute(0, 3, 1, 2)  # (batch, C, H, W)
+    resampled = torch.nn.functional.grid_sample(reshaped_images, grid.expand(reshaped_images.shape[0], -1, -1, -1), mode="bilinear", padding_mode="border", align_corners=True)
+    resampled_images = resampled.permute(0, 2, 3, 1).reshape(orig_shape)
+    return resampled_images
+
+
+def sample_frustum(dirs: torch.Tensor, poses: torch.Tensor, near: float, far: float, depth: int, batch_size: int, randomize: bool, device: torch.device, dtype: torch.dtype):
+    """
+    Sample points in the frustum of each camera.
+
+    Args:
+    - dirs: torch.Tensor of shape (N, H, W, 3)
+    - poses: torch.Tensor of shape (N, 4, 4)
+    - near: float
+    - far: float
+    - depth: int
+    - batch_size: int
+    - randomize: bool
+
+    Yields:
+    - points: torch.Tensor of shape (batch_size, depth, 3)
+    """
+
+    rays_d = torch.einsum('nij,nhwj->nhwi', poses[:, :3, :3], dirs)  # (N, H, W, 3)
+    rays_o = poses[:, None, None, :3, 3].expand(rays_d.shape)  # (N, H, W, 3)
+
+    rays_d = rays_d.reshape(-1, 3)  # (N*H*W, 3)
+    rays_o = rays_o.reshape(-1, 3)  # (N*H*W, 3)
+    num_rays = rays_d.shape[0]
+
+    depths = torch.linspace(near, far, steps=depth, device=device, dtype=dtype).unsqueeze(0)  # (1, depth)
+
+    if randomize:
+        indices = torch.randperm(num_rays, device=device)  # (N*H*W)
+    else:
+        indices = torch.arange(num_rays, device=device)  # (N*H*W)
+
+    for i in range(0, num_rays, batch_size):
+        batch_indices = indices[i:i + batch_size]
+        batch_rays_o = rays_o[batch_indices]  # (batch_size, 3)
+        batch_rays_d = rays_d[batch_indices]  # (batch_size, 3)
+
+        batch_depths = depths.clone()
+
+        if randomize:
+            midpoints = (depths[:, :-1] + depths[:, 1:]) / 2.0
+            noise = (torch.rand_like(midpoints) - 0.5) * (far - near) / depth
+            batch_depths[:, :-1] = midpoints + noise
+
+        batch_points = batch_rays_o[:, None, :] + batch_rays_d[:, None, :] * batch_depths[:, :, None]  # (batch_size, depth, 3)
+        yield batch_points, batch_depths, batch_indices
+
+
+def sample_random_frame(videos_data: torch.Tensor, batch_indices: torch.Tensor, device: torch.device, dtype: torch.dtype):
+    """
+    Sample a random frame from the given videos data.
+
+    Args:
+    - videos_data: torch.Tensor of shape (T, V, H, W, C)
+    - batch_indices: torch.Tensor of shape (batch_size)
+
+    Returns:
+    - batch_time: torch.Tensor of shape (1)
+    - batch_target_pixels: torch.Tensor of shape (batch_size, C)
+    """
+    frame = random.uniform(0, videos_data.shape[0] - 1)
+    frame_floor, frame_ceil, frames_alpha = int(frame), int(frame) + 1, frame - int(frame)
+    target_frame = (1 - frames_alpha) * videos_data[frame_floor] + frames_alpha * videos_data[frame_ceil]  # (V * H * W, C)
+    target_frame = target_frame.reshape(-1, 3)
+    batch_target_pixels = target_frame[batch_indices]  # (batch_size, C)
+    batch_time = torch.tensor(frame / (videos_data.shape[0] - 1), device=device, dtype=dtype)
+
+    return batch_time, batch_target_pixels
+
+
+def normalize_points(points: torch.Tensor, device: torch.device, dtype: torch.dtype):
+    """
+    Normalize the points to the range [0, 1].
+
+    Args:
+    - points: torch.Tensor of shape (..., 3)
+
+    Returns:
+    - points_normalized: torch.Tensor of shape (..., 3)
+    """
+
+    scene_min = torch.tensor([-20.0, -20.0, -20.0], device=device, dtype=dtype)
+    scene_max = torch.tensor([20.0, 20.0, 20.0], device=device, dtype=dtype)
+    points_normalized = (points - scene_min) / (scene_max - scene_min)
+    return points_normalized
+
+
+def shuffle_uv(focals: torch.Tensor, width: int, height: int, randomize: bool, device: torch.device, dtype: torch.dtype):
+    """
+    Shuffle the UV coordinates of the image plane.
+
+    Args:
+    - focals: torch.Tensor of shape (N)
+    - width: int
+    - height: int
+    - randomize: bool
+
+    Returns:
+    - dirs: torch.Tensor of shape (N, H, W, 3)
+    - u: torch.Tensor of shape (H, W)
+    - v: torch.Tensor of shape (H, W)
+    """
+    u, v = torch.meshgrid(torch.linspace(0, width - 1, width, device=device, dtype=dtype), torch.linspace(0, height - 1, height, device=device, dtype=dtype), indexing='xy')  # (H, W), (H, W)
+    if randomize:
+        du, dv = torch.rand_like(u), torch.rand_like(v)  # (H, W), (H, W)
+        u, v = torch.clip(u + du, 0, width - 1), torch.clip(v + dv, 0, height - 1)  # (H, W), (H, W)
+    u_normalized, v_normalized = (u - width * 0.5) / focals[:, None, None], (v - height * 0.5) / focals[:, None, None]  # (N, H, W), (N, H, W)
+    dirs = torch.stack([u_normalized, -v_normalized, -torch.ones_like(u_normalized)], dim=-1)  # (N, H, W, 3)
+
+    return dirs, u, v
+
+
 class PISGPipelineTorch:
     """
     Pipeline for training and testing the PISG model using PyTorch.
@@ -67,7 +221,7 @@ class PISGPipelineTorch:
         self.scheduler = torch.optim.lr_scheduler.ExponentialLR(self.optimizer, gamma=gamma)
 
         def PISG_forward(batch_points: torch.Tensor, batch_depths: torch.Tensor, batch_time: torch.Tensor):
-            batch_points_normalized = self.normalize_points(points=batch_points)  # (batch_size, depth, 3)
+            batch_points_normalized = normalize_points(points=batch_points, device=self.device, dtype=self.dtype)  # (batch_size, depth, 3)
             batch_input_xyzt_flat = torch.cat([batch_points_normalized, batch_time.expand(batch_points_normalized[..., :1].shape)], dim=-1).reshape(-1, 4)  # (batch_size * depth, 4)
             batch_rgb_map = self.query_rgb_map(xyzt=batch_input_xyzt_flat, batch_depths=batch_depths)
             return batch_rgb_map
@@ -76,7 +230,7 @@ class PISGPipelineTorch:
 
     def train(self, batch_size, save_ckp_path):
         videos_data = self.load_videos_data(*training_videos)
-        videos_data = self.resample_images_by_ratio_device(videos_data, self.ratio)
+        videos_data = resample_images_by_ratio_device(videos_data, self.ratio)
         videos_data = videos_data.permute(1, 0, 2, 3, 4)  # (T, V, H, W, C)
         poses, focals, width, height, near, far = self.load_cameras_data(*camera_calibrations)
         width = int(width * self.ratio)
@@ -84,11 +238,11 @@ class PISGPipelineTorch:
 
         import tqdm
         for _1 in tqdm.trange(0, 1):
-            dirs, u, v = self.shuffle_uv(focals=focals, width=width, height=height, randomize=True)
-            videos_data_resampled = self.resample_frames(frames=videos_data, u=u, v=v)  # (T, V, H, W, C)
+            dirs, u, v = shuffle_uv(focals=focals, width=width, height=height, randomize=True, device=self.device, dtype=self.dtype)
+            videos_data_resampled = resample_frames(frames=videos_data, u=u, v=v)  # (T, V, H, W, C)
 
-            for _2, (batch_points, batch_depths, batch_indices) in enumerate(tqdm.tqdm(self.sample_frustum(dirs=dirs, poses=poses, near=near, far=far, depth=self.depth, batch_size=batch_size, randomize=True), desc="Frustum Sampling")):
-                batch_time, batch_target_pixels = self.sample_random_frame(videos_data=videos_data_resampled, batch_indices=batch_indices)  # (batch_size, C)
+            for _2, (batch_points, batch_depths, batch_indices) in enumerate(tqdm.tqdm(sample_frustum(dirs=dirs, poses=poses, near=near, far=far, depth=self.depth, batch_size=batch_size, randomize=True, device=self.device, dtype=self.dtype), desc="Frustum Sampling")):
+                batch_time, batch_target_pixels = sample_random_frame(videos_data=videos_data_resampled, batch_indices=batch_indices, device=self.device, dtype=self.dtype)  # (batch_size, C)
                 batch_rgb_map = self.compiled_forward(batch_points, batch_depths, batch_time)
                 loss_image = torch.nn.functional.mse_loss(batch_rgb_map, batch_target_pixels)
                 self.optimizer.zero_grad()
@@ -119,9 +273,9 @@ class PISGPipelineTorch:
         test_timestamp = torch.tensor(target_timestamp / 120., device=self.device, dtype=self.dtype)
         import tqdm
         with torch.no_grad():
-            dirs, _, _ = self.shuffle_uv(focals=focals, width=width, height=height, randomize=False)
+            dirs, _, _ = shuffle_uv(focals=focals, width=width, height=height, randomize=False, device=self.device, dtype=self.dtype)
             rgb_map_list = []
-            for _1, (batch_points, batch_depths, batch_indices) in enumerate(tqdm.tqdm(self.sample_frustum(dirs=dirs, poses=poses, near=near, far=far, depth=self.depth, batch_size=batch_size, randomize=False), desc="Rendering Frame")):
+            for _1, (batch_points, batch_depths, batch_indices) in enumerate(tqdm.tqdm(sample_frustum(dirs=dirs, poses=poses, near=near, far=far, depth=self.depth, batch_size=batch_size, randomize=False, device=self.device, dtype=self.dtype), desc="Rendering Frame")):
                 batch_rgb_map = self.compiled_forward(batch_points, batch_depths, test_timestamp)
                 rgb_map_list.append(batch_rgb_map.clone())
             rgb_map_flat = torch.cat(rgb_map_list, dim=0)  # (H * W, 3)
@@ -211,154 +365,6 @@ class PISGPipelineTorch:
         focals = torch.tensor([info["focal"] * widths[0] / info["aperture"] for info in camera_infos], device=self.device, dtype=self.dtype)
 
         return poses, focals, widths[0], heights[0], nears[0], fars[0]
-
-    def resample_images_by_ratio_device(self, images: torch.Tensor, ratio: float):
-        """
-        resample images by ratio
-
-        Args:
-        - images: torch.Tensor of shape (V, T, H, W, C)
-        - ratio: float, resampling ratio
-
-        Returns:
-        - torch.Tensor of shape (V, T, H * ratio, W * ratio, C)
-
-        """
-        V, T, H, W, C = images.shape
-        images_permuted = images.permute(0, 1, 4, 2, 3).reshape(V * T, C, H, W)  # (V * T, C, H, W)
-        images_permuted_resized = (torch.nn.functional.interpolate(images_permuted, size=(int(H * ratio), int(W * ratio)), mode='bilinear', align_corners=False)
-                                   .reshape(V, T, C, int(H * ratio), int(W * ratio))
-                                   .permute(0, 1, 3, 4, 2))  # (V, T, H * ratio, W * ratio, C)
-        return images_permuted_resized
-
-    def resample_frames(self, frames: torch.Tensor, u: torch.Tensor, v: torch.Tensor):
-        """
-        Resample frames using the given UV coordinates.
-
-        Args:
-        - frames: torch.Tensor of shape (..., H, W, C)
-        - u: torch.Tensor of shape (N, H, W)
-        - v: torch.Tensor of shape (N, H, W)
-
-        Returns:
-        - resampled_images: torch.Tensor of shape (N, T, H, W, C)
-        """
-
-        H, W, C = frames.shape[-3:]
-        u_norm, v_norm = 2.0 * (u / (W - 1)) - 1, 2.0 * (v / (H - 1)) - 1
-        grid = torch.stack((u_norm, v_norm), dim=-1).unsqueeze(0)  # (1, H, W, 2)
-        orig_shape = frames.shape
-        reshaped_images = frames.reshape(-1, H, W, C).permute(0, 3, 1, 2)  # (batch, C, H, W)
-        resampled = torch.nn.functional.grid_sample(reshaped_images, grid.expand(reshaped_images.shape[0], -1, -1, -1), mode="bilinear", padding_mode="border", align_corners=True)
-        resampled_images = resampled.permute(0, 2, 3, 1).reshape(orig_shape)
-        return resampled_images
-
-    def sample_random_frame(self, videos_data: torch.Tensor, batch_indices: torch.Tensor):
-        """
-        Sample a random frame from the given videos data.
-
-        Args:
-        - videos_data: torch.Tensor of shape (T, V, H, W, C)
-        - batch_indices: torch.Tensor of shape (batch_size)
-
-        Returns:
-        - batch_time: torch.Tensor of shape (1)
-        - batch_target_pixels: torch.Tensor of shape (batch_size, C)
-        """
-        frame = random.uniform(0, videos_data.shape[0] - 1)
-        frame_floor, frame_ceil, frames_alpha = int(frame), int(frame) + 1, frame - int(frame)
-        target_frame = (1 - frames_alpha) * videos_data[frame_floor] + frames_alpha * videos_data[frame_ceil]  # (V * H * W, C)
-        target_frame = target_frame.reshape(-1, 3)
-        batch_target_pixels = target_frame[batch_indices]  # (batch_size, C)
-        batch_time = torch.tensor(frame / (videos_data.shape[0] - 1), device=self.device, dtype=self.dtype)
-
-        return batch_time, batch_target_pixels
-
-    def normalize_points(self, points: torch.Tensor):
-        """
-        Normalize the points to the range [0, 1].
-
-        Args:
-        - points: torch.Tensor of shape (..., 3)
-
-        Returns:
-        - points_normalized: torch.Tensor of shape (..., 3)
-        """
-
-        scene_min = torch.tensor([-20.0, -20.0, -20.0], device=self.device, dtype=self.dtype)
-        scene_max = torch.tensor([20.0, 20.0, 20.0], device=self.device, dtype=self.dtype)
-        points_normalized = (points - scene_min) / (scene_max - scene_min)
-        return points_normalized
-
-    def shuffle_uv(self, focals: torch.Tensor, width: int, height: int, randomize: bool):
-        """
-        Shuffle the UV coordinates of the image plane.
-
-        Args:
-        - focals: torch.Tensor of shape (N)
-        - width: int
-        - height: int
-        - randomize: bool
-
-        Returns:
-        - dirs: torch.Tensor of shape (N, H, W, 3)
-        - u: torch.Tensor of shape (H, W)
-        - v: torch.Tensor of shape (H, W)
-        """
-        u, v = torch.meshgrid(torch.linspace(0, width - 1, width, device=self.device, dtype=self.dtype), torch.linspace(0, height - 1, height, device=self.device, dtype=self.dtype), indexing='xy')  # (H, W), (H, W)
-        if randomize:
-            du, dv = torch.rand_like(u), torch.rand_like(v)  # (H, W), (H, W)
-            u, v = torch.clip(u + du, 0, width - 1), torch.clip(v + dv, 0, height - 1)  # (H, W), (H, W)
-        u_normalized, v_normalized = (u - width * 0.5) / focals[:, None, None], (v - height * 0.5) / focals[:, None, None]  # (N, H, W), (N, H, W)
-        dirs = torch.stack([u_normalized, -v_normalized, -torch.ones_like(u_normalized)], dim=-1)  # (N, H, W, 3)
-
-        return dirs, u, v
-
-    def sample_frustum(self, dirs: torch.Tensor, poses: torch.Tensor, near: float, far: float, depth: int, batch_size: int, randomize: bool):
-        """
-        Sample points in the frustum of each camera.
-
-        Args:
-        - dirs: torch.Tensor of shape (N, H, W, 3)
-        - poses: torch.Tensor of shape (N, 4, 4)
-        - near: float
-        - far: float
-        - depth: int
-        - batch_size: int
-        - randomize: bool
-
-        Yields:
-        - points: torch.Tensor of shape (batch_size, depth, 3)
-        """
-
-        rays_d = torch.einsum('nij,nhwj->nhwi', poses[:, :3, :3], dirs)  # (N, H, W, 3)
-        rays_o = poses[:, None, None, :3, 3].expand(rays_d.shape)  # (N, H, W, 3)
-
-        rays_d = rays_d.reshape(-1, 3)  # (N*H*W, 3)
-        rays_o = rays_o.reshape(-1, 3)  # (N*H*W, 3)
-        num_rays = rays_d.shape[0]
-
-        depths = torch.linspace(near, far, steps=depth, device=self.device, dtype=self.dtype).unsqueeze(0)  # (1, depth)
-
-        if randomize:
-            indices = torch.randperm(num_rays, device=self.device)  # (N*H*W)
-        else:
-            indices = torch.arange(num_rays, device=self.device)  # (N*H*W)
-
-        for i in range(0, num_rays, batch_size):
-            batch_indices = indices[i:i + batch_size]
-            batch_rays_o = rays_o[batch_indices]  # (batch_size, 3)
-            batch_rays_d = rays_d[batch_indices]  # (batch_size, 3)
-
-            batch_depths = depths.clone()
-
-            if randomize:
-                midpoints = (depths[:, :-1] + depths[:, 1:]) / 2.0
-                noise = (torch.rand_like(midpoints) - 0.5) * (far - near) / depth
-                batch_depths[:, :-1] = midpoints + noise
-
-            batch_points = batch_rays_o[:, None, :] + batch_rays_d[:, None, :] * batch_depths[:, :, None]  # (batch_size, depth, 3)
-            yield batch_points, batch_depths, batch_indices
 
 
 def test_pipeline(rank, gpu_size):
