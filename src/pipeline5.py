bc1a49ffd486143aa83e042e6cb755caa068a188
Xayah <ycxiang14sep@gmail.com>
2025-03-22 21:18:40 +0900

    initial test

--- src/pipeline5.py
+++ src/pipeline5.py
@@ -150,6 +150,56 @@ def load_cameras_data(*cameras_paths, device: torch.device, dtype: torch.dtype):
 # TO DELETE
 import numpy as np
 
+def pos_world2smoke(Pworld, w2s, scale_vector):
+    pos_rot = torch.sum(Pworld[..., None, :] * (w2s[:3, :3]), -1)  # 4.world to 3.target
+    pos_off = (w2s[:3, -1]).expand(pos_rot.shape)  # 4.world to 3.target
+    new_pose = pos_rot + pos_off
+    pos_scale = new_pose / (scale_vector)  # 3.target to 2.simulation
+    return pos_scale
+
+
+class BBox_Tool(object):
+    def __init__(self, smoke_tran_inv, smoke_scale, in_min=[0.15, 0.0, 0.15], in_max=[0.85, 1., 0.85], device=torch.device("cuda")):
+        self.s_w2s = torch.tensor(smoke_tran_inv, device=device, dtype=torch.float32).expand([4, 4])
+        self.s2w = torch.inverse(self.s_w2s)
+        self.s_scale = torch.tensor(smoke_scale.copy(), device=device, dtype=torch.float32).expand([3])
+        self.s_min = torch.tensor(in_min, device=device, dtype=torch.float32)
+        self.s_max = torch.tensor(in_max, device=device, dtype=torch.float32)
+
+    def world2sim(self, pts_world):
+        pts_world_homo = torch.cat([pts_world, torch.ones_like(pts_world[..., :1])], dim=-1)
+        pts_sim_ = torch.matmul(self.s_w2s, pts_world_homo[..., None]).squeeze(-1)[..., :3]
+        pts_sim = pts_sim_ / (self.s_scale)  # 3.target to 2.simulation
+        return pts_sim
+
+    def world2sim_rot(self, pts_world):
+        pts_sim_ = torch.matmul(self.s_w2s[:3, :3], pts_world[..., None]).squeeze(-1)
+        pts_sim = pts_sim_ / (self.s_scale)  # 3.target to 2.simulation
+        return pts_sim
+
+    def sim2world(self, pts_sim):
+        pts_sim_ = pts_sim * self.s_scale
+        pts_sim_homo = torch.cat([pts_sim_, torch.ones_like(pts_sim_[..., :1])], dim=-1)
+        pts_world = torch.matmul(self.s2w, pts_sim_homo[..., None]).squeeze(-1)[..., :3]
+        return pts_world
+
+    def sim2world_rot(self, pts_sim):
+        pts_sim_ = pts_sim * self.s_scale
+        pts_world = torch.matmul(self.s2w[:3, :3], pts_sim_[..., None]).squeeze(-1)
+        return pts_world
+
+    def isInside(self, inputs_pts):
+        target_pts = pos_world2smoke(inputs_pts, self.s_w2s, self.s_scale)
+        above = torch.logical_and(target_pts[..., 0] >= self.s_min[0], target_pts[..., 1] >= self.s_min[1])
+        above = torch.logical_and(above, target_pts[..., 2] >= self.s_min[2])
+        below = torch.logical_and(target_pts[..., 0] <= self.s_max[0], target_pts[..., 1] <= self.s_max[1])
+        below = torch.logical_and(below, target_pts[..., 2] <= self.s_max[2])
+        outputs = torch.logical_and(below, above)
+        return outputs
+
+    def insideMask(self, inputs_pts, to_float=True):
+        return self.isInside(inputs_pts).to(torch.float) if to_float else self.isInside(inputs_pts)
+
 
 def get_rays_np_continuous(H, W, c2w, K):
     # Generate random offsets for pixel coordinates
@@ -431,12 +481,16 @@ def get_velocity_and_derivitives(pts,
     return ret_list
 
 
-def get_raw(POINTS_TIME: torch.Tensor, DISTs: torch.Tensor, RAYs_D_FLAT: torch.Tensor, MODEL, ENCODER, device):
+def get_raw(POINTS_TIME: torch.Tensor, DISTs: torch.Tensor, RAYs_D_FLAT: torch.Tensor, BBOX_MODEL, MODEL, ENCODER, device):
     assert POINTS_TIME.dim() == 3 and POINTS_TIME.shape[-1] == 4
     assert POINTS_TIME.shape[0] == DISTs.shape[0] == RAYs_D_FLAT.shape[0]
     POINTS_TIME_FLAT = POINTS_TIME.view(-1, POINTS_TIME.shape[-1])  # [batch_size * N_depths, 4]
     out_dim = 1
-    POINTS_TIME_FLAT_FINAL = POINTS_TIME_FLAT
+    bbox_mask = BBOX_MODEL.insideMask(POINTS_TIME_FLAT[..., :3], to_float=False)
+    if bbox_mask.sum() == 0:
+        bbox_mask[0] = True
+        assert False
+    POINTS_TIME_FLAT_FINAL = POINTS_TIME_FLAT[bbox_mask]
 
     ## START
     POINTS_TIME_FLAT_FINAL.requires_grad = True
@@ -457,7 +511,8 @@ def get_raw(POINTS_TIME: torch.Tensor, DISTs: torch.Tensor, RAYs_D_FLAT: torch.T
     ret['_d_t'] = _d_t
     ## END
 
-    RAW_FLAT = raw_d
+    RAW_FLAT = torch.zeros([POINTS_TIME_FLAT.shape[0], out_dim], device=POINTS_TIME_FLAT.device, dtype=torch.float32)
+    RAW_FLAT[bbox_mask] = raw_d
     RAW = RAW_FLAT.reshape(*POINTS_TIME.shape[:-1], out_dim)
     assert RAW.dim() == 3 and RAW.shape[-1] == 1
 
@@ -544,6 +599,11 @@ if __name__ == '__main__':
         'near': NEAR_float,
         'far': FAR_float,
     }
+
+    VOXEL_TRAN_np = np.array([[ 1.0000000e+00,0.0000000e+00,7.5497901e-08,8.1816666e-02], [ 0.0000000e+00,1.0000000e+00,0.0000000e+00,-4.4627272e-02], [ 7.5497901e-08,0.0000000e+00,-1.0000000e+00,-4.9089999e-03], [ 0.0000000e+00,0.0000000e+00,0.0000000e+00,1.0000000e+00]])
+    VOXEL_SCALE_np = np.array([0.4909,0.73635,0.4909 ])
+    voxel_tran_inv = np.linalg.inv(VOXEL_TRAN_np)
+    BBOX_MODEL_gpu = BBox_Tool(voxel_tran_inv, VOXEL_SCALE_np)
     # TO DELETE
     # =============================================================================
 
@@ -571,7 +631,7 @@ if __name__ == '__main__':
                 POINTS_TIME_gpu = torch.cat([POINTS_gpu, TIME_STEP_gpu.expand(POINTS_gpu[..., :1].shape)], dim=-1)  # [batch_size, N_depths, 4]
 
                 ## START
-                RGB_MAP, _d_x, _d_y, _d_z, _d_t, pts, raw_d = get_raw(POINTS_TIME_gpu, DISTs_gpu, BATCH_RAYs_D_gpu, model_d, encoder_d, target_device)
+                RGB_MAP, _d_x, _d_y, _d_z, _d_t, pts, raw_d = get_raw(POINTS_TIME_gpu, DISTs_gpu, BATCH_RAYs_D_gpu, BBOX_MODEL_gpu, model_d, encoder_d, target_device)
 
                 raw_vel, raw_f = get_velocity_and_derivitives(pts, no_vel_der=True, **render_kwargs_train_vel)
                 _u_x, _u_y, _u_z, _u_t = None, None, None, None
